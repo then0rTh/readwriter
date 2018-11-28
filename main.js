bc5ba1e83119f17267b1bcea79add891d030e11f
then0rTh <nth.orx@gmail.com>
2018-11-28 09:17:48 +0100

    i

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-document.getElementById('status').innerHTML = 'status';
-document.getElementById('output-el').innerHTML = 'output-el';
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('worker.js')
-    .then(function (reg) {
-        console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch(function (error) {
-        console.log('Registration failed with ' + error);
-    });
-} else {
-    alert('no serviceWorker');
-}
+document.addEventListener('load', () => {
+    document.getElementById('status').innerHTML = 'status';
+    document.getElementById('output-el').innerHTML = 'output-el';
+});
 
-document.getElementById('status').innerHTML = 'nfc' in navigator ? 'found' : 'not found';
-navigator.nfc.watch((message) => {
-  const el = document.getElementById('status');
-  if (message.records[0].recordType == 'empty') {
-      el.innerHTML = 'empty';
-    // navigator.nfc.push({
-    //   url: "/custom/path",
-    //   records: [{ recordType: "text", data: 'Hello World' }]
-    // });
-  } else {
-    el.innerHTML = 'Read message written by ' + message.url;
-    processMessage(message);
-  }
-}).then(() => {
-    document.getElementById('status').innerHTML = "Added a watch.";
-    alert("Added a watch.");
-}).catch((error) => {
-    document.getElementById('status').innerHTML = "Adding watch failed: " + error.name;
-    alert("Adding watch failed: " + error.name);
+document.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('worker.js')
+        .then(function (reg) {
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function (error) {
+            console.log('Registration failed with ' + error);
+        });
+    } else {
+        alert('no serviceWorker');
+    }
+});
+
+document.addEventListener('load', () => {
+    document.getElementById('status').innerHTML = 'nfc' in navigator ? 'found' : 'not found';
+    navigator.nfc.watch((message) => {
+      const el = document.getElementById('status');
+      if (message.records[0].recordType == 'empty') {
+          el.innerHTML = 'empty';
+        // navigator.nfc.push({
+        //   url: "/custom/path",
+        //   records: [{ recordType: "text", data: 'Hello World' }]
+        // });
+      } else {
+        el.innerHTML = 'Read message written by ' + message.url;
+        processMessage(message);
+      }
+    }).then(() => {
+        document.getElementById('status').innerHTML = "Added a watch.";
+        alert("Added a watch.");
+    }).catch((error) => {
+        document.getElementById('status').innerHTML = "Adding watch failed: " + error.name;
+        alert("Adding watch failed: " + error.name);
+    });
 });
 
 function processMessage(message) {
